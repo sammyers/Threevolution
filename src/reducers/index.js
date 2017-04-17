3accf9fa4d2941cfb87e2b55654a3f8b7679a774
Sam Myers <sam@students.olin.edu>
2017-04-17 19:39:42 -0400

    Added webpack/react/redux scaffolding.

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { Map } from 'immutable';
+
+const rootReducer = (state = Map(), action) => {
+    switch (action) {
+        default:
+            return state;
+    }
+};
+
+export default rootReducer;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var { resolve } = require('path');
+
+var BUILD_DIR = resolve(__dirname, 'dist');
+var APP_DIR = resolve(__dirname, 'src');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:9090',
+        'webpack/hot/only-dev-server',
+        APP_DIR + '/index.jsx'
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'react-hot-loader/webpack!babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        hot: true,
+        contentBase: BUILD_DIR
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin()
+    ],
+};
